0756d398f7be95ddccee524a6948241bcfa34471
cbo <c.borges@castsoftware.com>
2019-05-24 10:28:25 -0700

    CLI for creating application on best node

--- aws_aip-scan.sh
+++ aws_aip-scan.sh
@@ -1,3 +1,5 @@
+command jq --version > /dev/null 2>&1 || {  echo "Missing jq command!"; exit 1;  }
+
 console_base_url=${console_base_url:-http://ec2-100-25-182-57.compute-1.amazonaws.com:8081}
 # GET token and cookies
 cookies=$(set -o pipefail; curl --connect-timeout 1 --max-time 3 -s -I -u cast:cast -XGET "$console_base_url"/api/user | sed -n -E 's/^Set-Cookie: (.*;) Path=.*$/\1/p' | paste -s -d ' ')
@@ -6,7 +8,7 @@ then
   printf "Fail to login with base url '%s'. Please check its availability.\n" "$console_base_url"
   exit 1
 fi
-printf "Cookies %s" "$cookies"; echo
+[ -n "$DEBUG" ] &&  { printf "Cookies %s" "$cookies"; echo;  } 1>&2
 
 #curl -s -u cast:cast -X GET --header "Cookie: $cookies" --header "X-XSRF-TOKEN: $(echo $cookies | cut -d ' ' -f1 | cut -d '=' -f2)" --header 'Content-Type: application/json' --header 'Accept: application/json' "$console_base_url"/api/ | python -mjson.tool
 
@@ -22,6 +24,14 @@ get_applications() {
   curl -s -u cast:cast -X GET --header "Cookie: $cookies" --header "X-XSRF-TOKEN: $(echo $cookies | cut -d ' ' -f1 | cut -d '=' -f2)" --header 'Content-Type: application/json' --header 'Accept: application/json' "$console_base_url"/api/applications | python -mjson.tool
 }
 
+get_accessible_nodes() {
+  get_nodes | jq 'map(select(.accessible))'
+}
+
+get_best_available_node() {
+  get_accessible_nodes | jq 'min_by(.numberOfApplications)|.guid'
+}
+
 post_job_add_version_data() {
 appName=${appName}; [ -z "$appName" ] && return 1;
 appGuid=${appGuid}; [ -z "$appGuid" ] && return 1;
@@ -35,7 +45,47 @@ JSON
 
 }
 
+job_payload() {
+jobType=${jobType}; [ -z "$jobType" ] && return 1;
+jobParametersFunc=${jobParametersFunc}; [ -z "$jobParametersFunc" ] && return 1;
 
+if declare -F "$jobParametersFunc" > /dev/null
+then
+  cat <<JSON
+{"jobType":"$jobType","jobParameters":$($jobParametersFunc)}
+JSON
+fi
+
+}
+
+job_declare_application_payload() {
+  declare_application_payload() {
+    cat <<JSON
+{"appName":"$name","nodeGuid":$(get_best_available_node),"domainName":null}
+JSON
+}
+
+jobType="declare_application" \
+  jobParametersFunc=declare_application_payload \
+  job_payload
+}
+
+create_application() {
+  local name=${name}; [ -z "$name" ] && echo "Application name missing" && exit 1;
+  curl "$console_base_url"/api/jobs -X POST  -s -u cast:cast --header "Cookie: $cookies" --header "X-XSRF-TOKEN: $(echo $cookies | cut -d ' ' -f1 | cut -d '=' -f2 | tr -d ';')" --header 'Content-Type: application/json' --header 'Accept: application/json'  -d @<(name="$name" job_declare_application_payload) | jq '.jobGuid'  | tr -d '"' | {
+  read -r jobGuid;
+  curl -s -u cast:cast -X GET --header "Cookie: $cookies" --header "X-XSRF-TOKEN: $(echo $cookies | cut -d ' ' -f1 | cut -d '=' -f2)" --header 'Content-Type: application/json' --header 'Accept: application/json' "$console_base_url"/api/jobs/"$jobGuid" | python -mjson.tool
+}
+
+}
+
+
+
+post_job2() {
+
+curl "http://ec2-100-26-49-152.compute-1.amazonaws.com:8081/api/jobs" -H "Cookie: JSESSIONID=BDCAC510CF45E030350F40C1E571E88D; XSRF-TOKEN=63ed0d96-3f6c-4839-85b3-e490601b8806" -H "Origin: http://ec2-100-26-49-152.compute-1.amazonaws.com:8081" -H "X-XSRF-TOKEN: 63ed0d96-3f6c-4839-85b3-e490601b8806" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US,en;q=0.9,de;q=0.8,fr;q=0.7" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36" -H "Content-Type: application/json;charset=UTF-8" -H "Accept: application/json, text/plain, */*" -H "Referer: http://ec2-100-26-49-152.compute-1.amazonaws.com:8081/ui/index.html" -H "X-Requested-With: XMLHttpRequest" -H "Connection: keep-alive" --data-binary "^{^\^"jobType^\^":^\^"declare_application^\^",^\^"jobParameters^\^":^{^\^"appName^\^":^\^"test4^\^",^\^"nodeGuid^\^":^\^"de27da64-e6cc-56b5-9ed8-e91eb648083b^\^",^\^"domainName^\^":null^}^}" --compressed
+
+}
 
 func=$1; shift
 if declare -F | cut -d ' ' -f3 | grep -q "^${func}$"
