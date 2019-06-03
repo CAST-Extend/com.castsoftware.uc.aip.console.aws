480ba8ae7441e5cfcbcbdde5d025a1334b360866
cbo <c.borges@castsoftware.com>
2019-06-03 05:16:28 -0700

    Add create version to api client

--- aws_aip-scan.sh
+++ aws_aip-scan.sh
@@ -1,7 +1,14 @@
-command jq --version > /dev/null 2>&1 || {  echo "Missing jq command!"; exit 1;  }
 
+# Check deps
+# jq
+command jq --version > /dev/null 2>&1 || {  echo "Missing jq command!"; exit 1;  }
+# curl
+
+
+# Base aip console url
 console_base_url=${console_base_url:-http://ec2-100-25-182-57.compute-1.amazonaws.com:8081}
-# GET token and cookies
+
+# Save cookies for next calls ( JSESSIONID and X-XSRF-TOKEN )
 cookies=$(set -o pipefail; curl --connect-timeout 1 --max-time 3 -s -I -u cast:cast -XGET "$console_base_url"/api/user | sed -n -E 's/^Set-Cookie: (.*;) Path=.*$/\1/p' | paste -s -d ' ')
 if (( $? != 0 ))
 then
@@ -10,24 +17,23 @@ then
 fi
 [ -n "$DEBUG" ] &&  { printf "Cookies %s" "$cookies"; echo;  } 1>&2
 
-#curl -s -u cast:cast -X GET --header "Cookie: $cookies" --header "X-XSRF-TOKEN: $(echo $cookies | cut -d ' ' -f1 | cut -d '=' -f2)" --header 'Content-Type: application/json' --header 'Accept: application/json' "$console_base_url"/api/ | python -mjson.tool
 
-#curl -u cast:cast -X PUT --header 'Cookie: {{cookie.stdout}}' --header 'X-XSRF-TOKEN: {{cookie.stdout.split(' ')[0].split('=')[1][:-1]}}' --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"database":{"host":"{{aip_postgres_ip}}","port":5432,"userName":"operator","password":"CastAIP"},"schemaName":"general_measure"}' 'http://localhost:8081/api/settings/measurement-settings'
-
-# create application
-
+# Get all nodes
 get_nodes() {
   curl -s -u cast:cast -X GET --header "Cookie: $cookies" --header "X-XSRF-TOKEN: $(echo $cookies | cut -d ' ' -f1 | cut -d '=' -f2)" --header 'Content-Type: application/json' --header 'Accept: application/json' "$console_base_url"/api/nodes | python -mjson.tool
 }
 
+# Get all applications
 get_applications() {
   curl -s -u cast:cast -X GET --header "Cookie: $cookies" --header "X-XSRF-TOKEN: $(echo $cookies | cut -d ' ' -f1 | cut -d '=' -f2)" --header 'Content-Type: application/json' --header 'Accept: application/json' "$console_base_url"/api/applications | python -mjson.tool
 }
 
+# Get all accessible nodes
 get_accessible_nodes() {
   get_nodes | jq 'map(select(.accessible))'
 }
 
+# Get node with less applications
 get_best_available_node() {
   get_accessible_nodes | jq 'min_by(.numberOfApplications)|.guid'
 }
@@ -74,9 +80,20 @@ create_application() {
   local name=${name}; [ -z "$name" ] && echo "Application name missing" && exit 1;
   curl "$console_base_url"/api/jobs -X POST  -s -u cast:cast --header "Cookie: $cookies" --header "X-XSRF-TOKEN: $(echo $cookies | cut -d ' ' -f1 | cut -d '=' -f2 | tr -d ';')" --header 'Content-Type: application/json' --header 'Accept: application/json'  -d @<(name="$name" job_declare_application_payload) | jq '.jobGuid'  | tr -d '"' | {
   read -r jobGuid;
-  curl -s -u cast:cast -X GET --header "Cookie: $cookies" --header "X-XSRF-TOKEN: $(echo $cookies | cut -d ' ' -f1 | cut -d '=' -f2)" --header 'Content-Type: application/json' --header 'Accept: application/json' "$console_base_url"/api/jobs/"$jobGuid" | python -mjson.tool
+  curl -s -u cast:cast -X GET --header "Cookie: $cookies" --header "X-XSRF-TOKEN: $(echo $cookies | cut -d ' ' -f1 | cut -d '=' -f2)" --header 'Content-Type: application/json' --header 'Accept: application/json' "$console_base_url"/api/jobs/"$jobGuid" | python -mjson.tool | tee $jobGuid
 }
 
+}
+
+create_version() {
+  local declare_job_result_file=$file; [ -z "$declare_job_result_file" ] && echo "Missing declare job result file" && exit 1;
+  cat "$declare_job_result_file" | jq '.appGuid' | tr -d '"' | {
+    read -r appGuid
+    curl "$console_base_url"/api/jobs -X POST  -s -u cast:cast --header "Cookie: $cookies" --header "X-XSRF-TOKEN: $(echo $cookies | cut -d ' ' -f1 | cut -d '=' -f2 | tr -d ';')" --header 'Content-Type: application/json' --header 'Accept: application/json'  -d @<(cat | tee create_version.payload <<JSON
+{"appGuid":"$appGuid","appName":"test1","versionName":"dd","releaseDate":"2019-05-26T19:29:24+02:00","fileName":"complicatedJava.zip","fileSize":1359462,"startStep":"unzip_source","endStep":"consolidate_snapshot"}
+JSON
+)
+  }
 }
 
 
@@ -94,3 +111,18 @@ then
 else
   printf "Usage : $0 %s" "$(declare -F | cut -d ' ' -f3 | paste -s -d '|')"; echo
 fi
+
+
+#curl "http://ec2-3-80-18-176.compute-1.amazonaws.com:8081/api/jobs" -H "Cookie: JSESSIONID=3789BB7CCB5EFF34D4029F32D4CEE4B0; XSRF-TOKEN=c21b8a7f-63c9-4078-af99-1b7dafd5d4cd" -H "X-XSRF-TOKEN: c21b8a7f-63c9-4078-af99-1b7dafd5d4cd" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US,en;q=0.9,de;q=0.8,fr;q=0.7" -H "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" -H "Accept: application/json, text/plain,
+#*/*" -H "Referer: http://ec2-3-80-18-176.compute-1.amazonaws.com:8081/ui/index.html" -H "X-Requested-With: XMLHttpRequest" -H "Connection: keep-alive" --compressed
+
+# create version
+
+
+
+# get filesize: du -b
+# upload file
+
+#curl "http://ec2-3-80-18-176.compute-1.amazonaws.com:8081/api/applications/922932c7-8ced-47f4-9269-2680123ae836/upload/47c2c3f4-3e5c-4016-9b80-174a35c12436" -X PATCH -H "Cookie: JSESSIONID=3789BB7CCB5EFF34D4029F32D4CEE4B0; XSRF-TOKEN=c21b8a7f-63c9-4078-af99-1b7dafd5d4cd" -H "Origin: http://ec2-3-80-18-176.compute-1.amazonaws.com:8081" -H "X-XSRF-TOKEN: c21b8a7f-63c9-4078-af99-1b7dafd5d4cd" -H "Accept-Encoding: gzip, deflate" -H "Accept-Language: en-US,en;q=0.9,de;q=0.8,fr;q=0.7" -H
+#"User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36" -H "Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryRYSwfkVBJywRWWX6" -H "Accept: application/json, text/plain, */*" -H "Referer: http://ec2-3-80-18-176.compute-1.amazonaws.com:8081/ui/index.html" -H "X-Requested-With: XMLHttpRequest" -H "Connection: keep-alive" --data-binary ^"------WebKitFormBoundaryRYSwfkVBJywRWWX6^
+
